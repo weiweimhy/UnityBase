c2f7d564a3de38d2a67e119947d47172fe1f52c5
weiwei <weiwei_mhy@163.com>
2018-10-28 20:54:05 +0800

    feature：新增objectExtension

---
+++ Assets/_Base/Extension/Csharp/objectExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseFramework
+{
+
+    public static class objectExtension
+    {
+
+        public static T As<T>(this object self) where T : class
+        {
+            return self as T;
+        }
+    }
+}
